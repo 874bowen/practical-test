0f549805eb2a5243e78846c414da77e0cd4625d8
Ivan Bowen <bowenivan16@gmail.com>
2024-04-17 13:15:00 +0300

    Add initialTaskState constant to constants.js

---
+++ resources/js/Constants/constants.js
@@ -0,0 +1,13 @@
+// export default initialTaskState ={
+//     title: "",
+//     description: "",
+//     priority_id: 1,
+// }
+
+const initialTaskState = {
+    title: "",
+    description: "",
+    priority_id: 1,
+}
+
+export default initialTaskState;
