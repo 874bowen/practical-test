1e71f2a2bdd7519e3876fdda8a7d264b5011e8a8
Ivan Bowen <bowenivan16@gmail.com>
2024-04-17 13:01:40 +0300

    db(seeder): Register PrioritySeeder to DatabaseSeeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -18,6 +18,11 @@ class DatabaseSeeder extends Seeder
         User::factory()->create([
             'name' => 'Test User',
             'email' => 'test@example.com',
+            'password' => '12345678',
+        ]);
+
+        $this->call([
+            PrioritySeeder::class,
         ]);
     }
 }
